e54b7a8f04c2a996bcee56c5eb0f564a9963699a
sks981 <9758991320@qq.com>
2026-04-14 21:52:17 +0800

    feat: 添加鼠标滚动缩放功能，修改电视图像和墙壁样式

---
+++ web/start_server.py
@@ -0,0 +1,9 @@
+import http.server
+import socketserver
+
+PORT = 8888
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(('', PORT), Handler) as httpd:
+    print('Server running at http://localhost:' + str(PORT))
+    httpd.serve_forever()
